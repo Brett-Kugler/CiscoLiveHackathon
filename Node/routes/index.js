22ca0d83d06fc2d3da455f31f60519f3a05a75a7
Harvey Chan <ch4ch4@gmail.com>
2014-05-21 08:48:02 -0700

    made a nicer sms notification

--- Node/routes/index.js
+++ Node/routes/index.js
@@ -12,34 +12,23 @@ var Datastore = require('nedb')
 // You can issue commands right away
 
 /* GET home page. */
-router.get('/', function(req, res) {
-    res.render('index', { title: 'Express' });
-});
+//router.get('/', function(req, res) {
+//    res.render('index', { title: 'Express' });
+//});
 
-router.get('/test', function (req, res) {
-    res.end('{"success":"true"}');
-});
+//router.get('/test', function (req, res) {
+//    res.end('{"success":"true"}');
+//});
 
-router.get('/insertTestEvent', function (req, res) {
+//router.get('/insertTestEvent', function (req, res) {
+//    db.insert(req.query, function (err, newDocs) {
+//        console.log(newDocs);
+//        // Two documents were inserted in the database
+//        // newDocs is an array with these documents, augmented with their _id
+//    });
 
-    //db.insert([{ a: 5 }, { a: 42 }], function (err, newDocs) {
-    //    // Two documents were inserted in the database
-    //    // newDocs is an array with these documents, augmented with their _id
-    //});
-
-    //// Finding all inhabited planets in the solar system
-    //db.find({ system: 'solar', inhabited: true }, function (err, docs) {
-    //    // docs is an array containing document Earth only
-    //});
-
-    db.insert(req.query, function (err, newDocs) {
-        console.log(newDocs);
-        // Two documents were inserted in the database
-        // newDocs is an array with these documents, augmented with their _id
-    });
-
-    res.end('{"success":"true"}');
-});
+//    res.end('{"success":"true"}');
+//});
 
 router.get('/dropDB', function (req, res) {
 
@@ -143,7 +132,7 @@ router.get('/testParseCMX', function (req, res) {
     });
 });
 
-//http://localhost:3000/findMAC?mac=00:27:10:d3:51:78
+//http://localhost:3000/findMAC?mac=3c:a9:f4:53:2d:cc
 router.get('/findMAC', function (req, res) {
     var macAddress = req.query.mac;
 
@@ -227,7 +216,7 @@ router.get('/saveUserContacts', function (req, res) {
 
 router.get('/quake', function (req, res) {
     var isQuake = req.query.quake;
-
+    //var cmxUserInfo;
     if (!isQuake) {
         res.end('{"quake":' + isQuake + '}');
     }
@@ -236,42 +225,33 @@ router.get('/quake', function (req, res) {
         var notifiedUsers = [];
         function pushArrayElements(element, index, array) {
             console.log("Notifying contacts for user: " + JSON.stringify(element)); //for debug
+            //var cmxUserInfo;
             
-            db.find({ 'username': element.username, 'type': 'contact' }, function (err, contacts) {
-                var smsNumbers = [];
-                function pushContactElements(element, index, array) {
-                    smsNumbers.push(element);
+            getCMXInfoByMac(element.mac, function (cmxUserInfo) {
+                console.log('mac: ' + element.mac);
+                console.log('cmx:' + cmxUserInfo);
+                console.log('username:' + element.username);
 
-                    //send sms here
-                    //Send an SMS text message
-                    twilioclient.sendMessage({
+                db.find({ 'username': element.username, 'type': 'contact' }, function (err, contacts) {
+                    var smsNumbers = [];
+                    function pushContactElements(element, index, array) {
+                        //send sms here
+                        //Send an SMS text message
+                        sendSMS(cmxUserInfo, element);
 
-                        to: element.telephone, // Any number Twilio can deliver to
-                        from: '+14154834220', // A number you bought from Twilio and can use for outbound communication
-                        body: 'Earthquake Alert!' // body of the SMS message
-
-                    }, function (err, responseData) { //this function is executed when a response is received from Twilio
-
-                        if (!err) { // "err" is an error received during the request, if any
-
-                            // "responseData" is a JavaScript object containing data received from Twilio.
-                            // A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
-                            // http://www.twilio.com/docs/api/rest/sending-sms#example-1
-
-                            console.log(responseData.from); // outputs "+14506667788"
-                            console.log(responseData.body); // outputs "word to your mother."
-
-                        }
-                    });
-                }
-                users.forEach(pushContactElements);
-
-                if (smsNumbers.count == contacts.count) {
-                    if (notifiedUsers.count == users.count) {
-                        res.end('{"quake":' + isQuake + '}');
+                        smsNumbers.push(element);
                     }
-                }
+                    users.forEach(pushContactElements);
+
+                    if (smsNumbers.count == contacts.count) {
+                        if (notifiedUsers.count == users.count) {
+                            res.end('{"quake":' + isQuake + '}');
+                        }
+                    }
+                });
             });
+            
+            //console.log(cmxUserInfo);
 
             notifiedUsers.push(element);
         }
@@ -282,5 +262,64 @@ router.get('/quake', function (req, res) {
         //}
     });
 });
+
+function getCMXInfoByMac(mac, callback) {
+    request.get({
+        uri: 'https://10.10.20.21/api/contextaware/v1/location/clients/',
+        auth: {
+            user: 'devuser',
+            pass: 'devuser',
+            sendImmediately: true
+        }
+    }, function (e, r, body) {
+        try {
+            parseString(body, function (err, result) {
+                var clients = [];
+
+                function pushArrayElements(element, index, array) {
+                    //console.log("client[" + index + "] = " + JSON.stringify(element)); //for debug
+                    clients.push(element);
+                    if (element.$.macAddress == mac) {
+                        callback(element);
+                    }
+                }
+                result.Locations.WirelessClientLocation.forEach(pushArrayElements);
+
+                if (result.Locations.WirelessClientLocation.count == clients.count) {
+                    //res.end('{"success":"false"}');
+                    callback(null);
+                }
+            });
+        } catch (e) {
+            console.log("error: " + e);
+            callback(null);
+            //res.end('{"success":"false"}');
+        }
+    });
+}
+
+function sendSMS(cmxUserInfo, user) {
+    console.log('sending sms about: ' + JSON.stringify(cmxUserInfo));
+    console.log('sending sms to: ' + JSON.stringify(user));
+
+    if(cmxUserInfo != null) twilioclient.sendMessage({
+
+        to: user.telephone, // Any number Twilio can deliver to
+        from: '+14154834220', // A number you bought from Twilio and can use for outbound communication
+        body: 'Earthquake Alert! The last known location of ' + user.username + ' is here: ' + 'https://www.google.com/maps/?saddr=' + cmxUserInfo.GeoCoordinate[0].$.lattitude + ',' + cmxUserInfo.GeoCoordinate[0].$.longitude // body of the SMS message
+
+    }, function (err, responseData) { //this function is executed when a response is received from Twilio
+
+        if (!err) { // "err" is an error received during the request, if any
+
+            // "responseData" is a JavaScript object containing data received from Twilio.
+            // A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
+            // http://www.twilio.com/docs/api/rest/sending-sms#example-1
+
+            console.log(responseData.from); // outputs "+14506667788"
+            console.log(responseData.body); // outputs "word to your mother."
+        }
+    });
+}
 
 module.exports = router;
